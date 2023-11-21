7e4f67981878cdeb5dd51419e497e0404be8f923
jenny <jenny.liao@ashbury.ca>
2023-11-20 20:50:06 -0500

    Tried to implement blueJ code

--- src/Player.java
+++ src/Player.java
@@ -14,30 +14,43 @@ public class Player extends Entity{
         lvl = level;
 
     }
+    public Player(int level, PVector pos) {
+        this.position= pos;
+        this.velocity = new PVector(5, 5);
+        this.rotation = 0;
+        lvl = level;
+
+    }
     public void accelerate() {
-        if(velocity.getSize() <= 2.5) {
+
+        if(velocity.getSize() <= 15) {
             velocity.setSize(velocity.getSize()+0.05);
         }
         else{
             velocity.setSize(velocity.getSize());
         }
         velocity.setAngle(rotation);
+        rotation = velocity.getAngle();     //testing
+
+
     }
     public void deccelerate() {
         velocity.setSize(velocity.getSize()-0.01);
     }
     public void turnRight() {
-        rotation += 1;
+        rotation += 5;
     }
     public void turnLeft() {
-        rotation -= 1;
+        rotation -= 5;
+    }
+    public double getRotation() {
+        return rotation;
     }
 
 
     @Override
     public void move() {
 
-        System.out.println("moving person testing");
         position.add(velocity.getX(), velocity.getY());
     }
 
